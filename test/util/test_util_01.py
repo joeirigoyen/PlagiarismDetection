453a1402a572268cdf75368089bde27f68512ea7
RebecaRojas <A01751192@itesm.mx>
2023-04-21 02:47:44 -0600

    unit tests for util

--- test/util/test_util_01.py
+++ test/util/test_util_01.py
@@ -2,10 +2,10 @@ import os
 import tempfile
 import unittest
 from pathlib import Path
-import src.util.file_manager as fm
+from unittest.mock import patch
+from src.util.file_manager import UnitaryFile, FileManager
 
-
-class Testfm(unittest.TestCase):
+class TestUnitaryFile(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.TemporaryDirectory()
         self.temp_file = tempfile.NamedTemporaryFile(delete=False, dir=self.temp_dir.name)
@@ -18,27 +18,63 @@ class Testfm(unittest.TestCase):
     def test_convert_to_path(self):
         file_path_str = "path/to/file.txt"
         file_path = Path(file_path_str)
-        self.assertEqual(fm.convert_to_path(file_path_str), file_path)
-        self.assertEqual(fm.convert_to_path(file_path), file_path)
+        self.assertEqual(UnitaryFile.convert_to_path(file_path_str), file_path)
+        self.assertEqual(UnitaryFile.convert_to_path(file_path), file_path)
 
     def test_exists(self):
-        file = fm(self.temp_path / "existing_file.txt")
+        file = UnitaryFile(self.temp_path / "existing_file.txt")
         self.assertTrue(file.exists())
 
-        file = fm(self.temp_path / "non_existing_file.txt")
+        file = UnitaryFile(self.temp_path / "non_existing_file.txt")
         self.assertFalse(file.exists())
 
     def test_create_file(self):
-        file = fm(self.temp_path / "new_file.txt")
+        file = UnitaryFile(self.temp_path / "new_file.txt")
         self.assertFalse(file.exists())
         file.create()
         self.assertTrue(file.exists())
-        self.assertTrue(os.path.isfile(str(file._file)))
-        os.remove(str(file._file))
+        self.assertTrue(os.path.isfile(str(file._f)))
+        os.remove(str(file._f))
 
     def test_create_directory(self):
-        dir = fm(self.temp_path / "new_directory")
+        dir = UnitaryFile(self.temp_path / "new_directory")
         self.assertFalse(dir.exists())
         dir.create()
         self.assertTrue(dir.exists())
-        self.assertTrue(os.path.isdir(str(dir._file)))
+        self.assertTrue(os.path.isdir(str(dir._f)))
+
+
+class TestFileManager(unittest.TestCase):
+
+    def setUp(self):
+        self.file_manager = FileManager()
+
+    def test_validate_file_existing_file(self):
+        # Arrange
+        file_path = "test/resources/file.txt"
+
+        # Act
+        result = self.file_manager.validate_file(file_path)
+
+        # Assert
+        self.assertTrue(result)
+
+    def test_validate_file_non_existing_file(self):
+        # Arrange
+        file_path = "test/resources/file.txt"
+
+        # Act
+        result = self.file_manager.validate_file(file_path, create=False)
+
+        # Assert
+        self.assertFalse(result)
+
+    def test_validate_file_create_file(self):
+        # Arrange
+        file_path = "test/resources/file.txt"
+
+        # Act
+        result = self.file_manager.validate_file(file_path, create=True)
+
+        # Assert
+        self.assertTrue(result)
