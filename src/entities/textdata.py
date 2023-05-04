2b11913d0517b952ab5556f77a4edfc5a2eca622
Eduardo Rodríguez <edurdgzl@hotmail.com>
2023-05-03 22:07:56 -0600

    Update textdata.py

--- src/entities/textdata.py
+++ src/entities/textdata.py
@@ -8,6 +8,7 @@ import nltk
 
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 from os import path
 from pathlib import Path
 from src.util.file_manager import FileManager as Fm
@@ -21,12 +22,6 @@ class TextData:
         """
         self._original = data
         self.data = data
-
-    def __reset(self) -> None:
-        """
-        Reset the data to the original state.
-        """
-        self.data = self._original
 
     def __download_nltk_stopwords(self, dev: bool = False) -> None:
         """
@@ -42,7 +37,6 @@ class TextData:
         Remove punctuation from the data.
         :return:
         """
-        self.__reset()
         self.data = self.data.translate(str.maketrans('', '', punctuation))
         return self.data
 
@@ -50,29 +44,100 @@ class TextData:
         """
         Remove stopwords from the data.
         """
-        self.__reset()
         self.__download_nltk_stopwords(dev)
         self.data = ' '.join([word for word in self.data.split() if word not in stopwords.words('english')])
         return self.data
 
-    def lemmatize(self):
+    def lemmatize(self) -> set:
         """
         Lemmatize the data.
         """
-        pass
+        lemmatizer = WordNetLemmatizer()
+        words = nltk.word_tokenize(self.remove_punctuation(self))
+        lemms = []
+        for word in words:
+            lemms.append(lemmatizer.lemmatize(word))
+        return set(lemms)
 
-    def stem(self):
+    def stem(self) -> set:
         """
         Stem the data.
         """
         stemmer = PorterStemmer()
-        words = nltk.word_tokenize(self.remove_punctuation(self.data))
+        words = nltk.word_tokenize(self.remove_punctuation(self))
         stems = []
         for word in words:
             stems.append(stemmer.stem(word))
         return set(stems)
 
 
-if __name__ == '__main__':
-    textdata = TextData("Hello, people of the world! I love everyone!")
-    textdata.remove_stopwords()
+class TextDataDirectory:
+    def __init__(self, data: str | Path) -> None:
+        """
+        Class that contains a directory of text data.
+        """
+        self.data: list[TextData] = self.__convert_directory(data)
+
+    @staticmethod
+    def __validate_dir(data: str | Path) -> bool:
+        datapath = Path(data)
+        return datapath.exists() and datapath.is_dir()
+
+    def __convert_directory(self, data: str | Path) -> list[TextData]:
+        """
+        Convert the data directory into a list of TextData objects.
+        """
+        textdata_list = []
+        if self.__validate_dir(data):
+            for file in Path(data).iterdir():
+                if file.is_file() and file.suffix == ".txt":
+                    textdata_list.append(TextData(Fm.read_file(file)))
+            return textdata_list
+        else:
+            raise FileNotFoundError("The specified path does not exist or is not a directory.")
+
+    def __download_nltk_stopwords(self, dev: bool = False) -> None:
+        """
+        Download the nltk stopwords.
+        """
+        dev_root = Path(path.dirname(path.abspath(__file__))).parent.parent.joinpath("self") / "stopwords"
+        normal_root = Path(path.dirname(path.abspath(__file__))).parent / "stopwords"
+        root = dev_root if dev else normal_root
+        Fm.download_nltk_stopwords(root)
+
+    def remove_punctuation(self) -> list[TextData]:
+        """
+        Remove punctuation from the data.
+        :return:
+        """
+        for textdata in self.data:
+            textdata.remove_punctuation()
+        return self.data
+
+    def remove_stopwords(self, dev=False) -> list[TextData]:
+        """
+        Remove stopwords from the data.
+        """
+        self.__download_nltk_stopwords(dev)
+        for textdata in self.data:
+            textdata.remove_stopwords()
+        return self.data
+
+    def lemmatize(self) -> list[set]:
+        """
+        Lemmatize the data.
+        """
+        lemms = []
+        for textdata in self.data:
+            lemms.append(textdata.lemmatize())
+        return lemms
+
+
+    def stem(self) -> list[set]:
+        """
+        Stem the data.
+        """
+        stems = []
+        for textdata in self.data:
+            stems.append(textdata.stem())
+        return stems
